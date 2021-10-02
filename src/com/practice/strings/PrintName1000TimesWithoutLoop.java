34621f1100e203b04b8df685005bfa2c987d19a9
Dilip Singh <dilipsingh1306@gmail.com>
2021-10-02 13:39:08 +0530

    Practice Files

---
+++ src/com/practice/strings/PrintName1000TimesWithoutLoop.java
@@ -0,0 +1,18 @@
+package com.practice.strings;
+
+public class PrintName1000TimesWithoutLoop {
+
+	public static void main(String[] args) {
+		
+		String temp = "i";
+		
+		temp = temp.replace("i", "iiiiiiiiii");
+		temp = temp.replace("i", "iiiiiiiiii");
+		temp = temp.replace("i", "iiiiiiiiii");
+		
+		String name = temp.replace("i", "Dilip Singh \n");
+		System.out.println(name);
+
+	}
+
+}
